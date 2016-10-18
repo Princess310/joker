03dbc909235c48a57242dc3753f8efce981e9041
Princess310 <837478514@qq.com>
2016-10-18 12:38:39 +0800

    . fix db connect wrong

--- src/main/webapp/WEB-INF/sql/00_create-db.sql
+++ src/main/webapp/WEB-INF/sql/00_create-db.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE IF EXISTS mvc;
--- DROP USER IF EXISTS pmvc_user;
+-- DROP USER IF EXISTS joker_user;
 CREATE USER joker_user PASSWORD 'welcome';
-CREATE DATABASE joker_db owner pmvc_user ENCODING = 'UTF-8';
+CREATE DATABASE joker_db owner joker_user ENCODING = 'UTF-8';
 
 
