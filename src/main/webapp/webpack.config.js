d06f30e2ba0154fd731b6fa8b1e19facc22e840b
Princess310 <837478514@qq.com>
2016-10-19 13:28:35 +0800

    . add node env

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,30 +1,47 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-		entry: {
-			app: './app.js'
-		},
-		output: {
-			path: './dist/',
-			publicPath: "./dist/",
-			filename: '[name].js'
-		},
-		plugins: [
-			new webpack.optimize.OccurrenceOrderPlugin(),
-			new webpack.optimize.DedupePlugin(),
-		],
-		module: {
-			loaders: [
-					{ test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader' },
-					{ test: /\.less$/, loader: 'style!css!less' },
-					{ test: /.*\.(gif|png|jpe?g|svg)$/i, loader: 'url' },
-					{ test: /\.(woff|woff2)$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-					{ test: /\.ttf$|\.eot$|\.svg$/, loader: 'file-loader' }
-			]
-		},
-		resolve: {
-				modulesDirectories: ['webapp', 'node_modules'],
-				extensions: ['', '.js', '.jsx']
-		}
-};
+const NODE_ENV = process.env.NODE_ENV;
+
+let config = {
+	entry: {
+		app: './app.js'
+	},
+	output: {
+		path: './dist/',
+		publicPath: "./dist/",
+		filename: '[name].js'
+	},
+	plugins: [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.optimize.DedupePlugin(),
+	],
+	module: {
+		loaders: [
+				{ test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader' },
+				{ test: /\.less$/, loader: 'style!css!less' },
+				{ test: /.*\.(gif|png|jpe?g|svg)$/i, loader: 'url' },
+				{ test: /\.(woff|woff2)$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+				{ test: /\.ttf$|\.eot$|\.svg$/, loader: 'file-loader' }
+		]
+	},
+	resolve: {
+			modulesDirectories: ['webapp', 'node_modules'],
+			extensions: ['', '.js', '.jsx']
+	}
+}
+
+
+if (NODE_ENV === 'production') {
+	config.plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.optimize.DedupePlugin()
+	);
+}
+
+module.exports = config;
